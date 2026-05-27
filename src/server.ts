3ec6e4d97efd44edbe00bb88447fcc47855accd0
Alex <theeduazizjon@gmail.com>
2026-05-27 16:40:41 +0900

    Reset repo to Burak project only

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+console.log("Executed");
+
+import moment from 'moment';
+
+const currentTime = moment().format("YYYY MM DD");
+console.log(currentTime);
+
+const person: string = "Martin"
+const count: number = 100; 
+
+// Architectural pattern: MVC, DI(dependency injection), MVP
+
+// MVC = Model View Controller 
+
+// Design pattern: Middleware, Decorator

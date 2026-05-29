fb850531140f2093ff13d1d79bcc46165cc308b1
Alex <theeduazizjon@gmail.com>
2026-05-29 13:30:11 +0900

    feat: Connected MongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -5,4 +5,13 @@ dotenv .config();
 
 // console.log("MONGO_URL:", process.env.MONGO_URL);
 
-// CLUSTER => DATABASE => COLLECTION => DOCUMENT 
+// CLUSTER => DATABASE => COLLECTION => DOCUMENT
+
+import mongoose from 'mongoose'
+
+mongoose.connect(process.env.MONGO_URL as string, {})
+.then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+}) 
+.catch(err => console.log("ERROR on MongoDB", err));

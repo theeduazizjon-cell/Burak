d2b63c71b554a3dd6e046746f6ad13733b70641b
Alex <theeduazizjon@gmail.com>
2026-06-02 23:27:19 +0900

    feat: create routing and member controllers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express"; 
 import path from "path";
+import router from "./router"; 
 
 /* 1-ENTRANCE */
 const app = express();
@@ -12,7 +13,9 @@ app.use(express.json());
 /* 3 -VIEWS */ 
 app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', "ejs");
+
 /* 4-ROUTERS */
+app.use("/",router); // Middleware Design Pattern 
 
 export default app;  // module.exports
 
---
+++ src/router.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const router = express.Router(); 
+import memberController from "./controllers/member.controller";
+
+router.get('/', memberController.goHome); 
+
+router.get('/login', memberController.goLogin); 
+
+router.get('/signup',memberController.getSignup); 
+
+export default router; // modele.exports

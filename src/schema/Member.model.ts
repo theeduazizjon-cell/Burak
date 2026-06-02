3deb42d20cd7168442095cce2ffaa6da305937f5
Alex <theeduazizjon@gmail.com>
2026-06-03 00:12:48 +0900

    feat: developed member schema model

---
+++ src/schema/Member.model.ts
@@ -0,0 +1,54 @@
+import mongoose, {Schema} from "mongoose"; 
+import { MemberStatus, MemberType } from "../libs/enums/member.enum";
+
+// Schema First & Code first
+
+const memberSchema = new Schema({
+    memberType: {
+        type:String, 
+        enum:MemberType, 
+        defaulr:MemberType.USER
+    }, 
+    memberStatus: {
+        type:String, 
+        enum:MemberStatus, 
+        default:MemberStatus.ACTIVE
+    },
+
+    memberNick: {
+        type:String,
+        index: { unique: true, sparse: true }
+        required: true
+    }, 
+
+    memberPhone: {
+        type:String,
+        index: { unique: true, sparse: true }
+        required: true
+    }, 
+
+    memberPassword: {
+        type:String,
+        select: false,
+        required: true
+    },
+
+    memberAddress: {
+        type:String,
+    },
+
+    memberDesc: {
+        type:String,
+    }, 
+
+    memberImnage:{
+        type:String,
+    }, 
+
+    memberPoints: {
+        type:Number,
+        default:0
+    }, 
+}, 
+{ timestamps: true } // updatedAt, createdAt
+);

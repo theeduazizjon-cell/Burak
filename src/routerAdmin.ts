d157b31f82af1938c5e2b610186e124e9ab9dd23
Alex <theeduazizjon@gmail.com>
2026-06-05 12:36:19 +0900

    fix: modifications on APIs

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -2,6 +2,7 @@ import express from 'express';
 const routerAdmin = express.Router(); 
 import restaurantController from "./controllers/restaurant.controller";
 
+// Restaurant members // 
 routerAdmin.get('/', restaurantController.goHome); 
 
 routerAdmin
@@ -11,5 +12,9 @@ routerAdmin
 routerAdmin
 .get('/signup',restaurantController.getSignup)
 .post('/signup', restaurantController.processSignup); 
+ 
 
+// Products // 
+
+// User //
 export default routerAdmin; // modele.exports

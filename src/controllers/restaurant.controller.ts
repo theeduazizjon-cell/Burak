ad27f4891529a196f777a5626d19aa15ddcecb20
Alex <theeduazizjon@gmail.com>
2026-06-05 12:08:32 +0900

    feat: created project standards

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -5,6 +5,10 @@ import MemberService from "../models/Member.service";
 const restaurantController: T = {}; 
 restaurantController.goHome = (req: Request, res:Response) => {
     try {
+          console.log("goHome");
+          // LOGIC 
+          // Service Model
+          // ... 
           res.send("Home Page");
     } catch (err){
         console.log("Error, goHome:", err);
@@ -13,6 +17,7 @@ restaurantController.goHome = (req: Request, res:Response) => {
 
 restaurantController.goLogin = (req: Request, res:Response) => {
     try {
+          console.log("goLogin");
           res.send("Login Page");
     } catch (err){
         console.log("Error, goLogin:", err);
@@ -21,6 +26,7 @@ restaurantController.goLogin = (req: Request, res:Response) => {
 
 restaurantController.getSignup = (req: Request, res:Response) => {
     try {
+            console.log("getSignup");
           res.send("Signup Page");
     } catch (err){
         console.log("Error, getSignup:", err);
---
+++ src/train.ts
@@ -0,0 +1,11 @@
+/*
+Project Standards: 
+ - Logging standards
+ - Naming standards 
+    function, method, variable => CAMEL 
+    class => PASCAL 
+    fodler => KEBAB 
+    css => SNAKE 
+ - Error handling standards
+
+*/

8b13a4350ff8716081108cda806c5f9ad572800c
Alex <theeduazizjon@gmail.com>
2026-06-10 15:28:51 +0900

    fix: some algorithmic test on train.ts file

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,29 @@
+// TASK Q:
+
+/*
+Masalaning sharti:
+
+Shunday function yozing, u 2 ta parametrga ega bo'lib
+birinchisi object, ikkinchisi string bo'lsin.
+Agar qabul qilinayotgan ikkinchi string, objectning
+biror bir propertysiga mos kelsa, 'true', aks holda mos kelmasa 'false' qaytarsin.
+
+
+MASALAN: hasProperty({ name: "BMW", model: "M3" }, "model"); return true;
+Ushbu misolda, 'model' string, objectning propertysiga mos kelganligi uchun 'true' natijani qaytarmoqda
+*/ 
+
+// Masalaning yechimi: 
+
+function hasProperty(obj: object, prop: string): boolean {
+    return Object.prototype.hasOwnProperty.call(obj, prop);
+}
+
+console.log(hasProperty({ name: "BMW", model: "M3" }, "model"));  // true
+console.log(hasProperty({ name: "BMW", model: "M3" }, "color"));  // false
+
+
+
 /*
 Project Standards: 
  - Logging standards

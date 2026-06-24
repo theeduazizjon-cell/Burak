67ea97bb72a5622b244654b1e8809ace427c5895
Alex <theeduazizjon@gmail.com>
2026-06-24 16:15:55 +0900

    feat: signup process FrontEnd validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,25 @@
-console.log("Signup frontend javascript file");
+console.log("Signup frontend javascript file"); 
+
+$(function () {}); 
+
+function validateSignUpForm() {
+    const memberNick = $(".member-nick").val(); 
+    const memberPhone = $(".member-phone").val(); 
+    const memberPassword = $(".member-password").val(); 
+    const confirmPassoword = $(".confirm-password").val(); 
+
+    if(
+        memberNick === "" || 
+        memberPhone === "" ||
+        memberPassword === "" ||
+        confirmPassowrd === "" 
+    ) {
+        alert("Please insert all required inputs"); 
+        return false; 
+    }
+    if (memberPassword !== confirmPassoword) {
+        alert("Password differs, please check!"); 
+          return false;
+    }
+   
+}

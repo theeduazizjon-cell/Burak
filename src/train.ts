77807fbb7009b40ce211bea671279fc191a687b8
Alex <theeduazizjon@gmail.com>
2026-06-24 16:37:54 +0900

    fix: new logic testing on train.ts(TASK-V)

--- src/train.ts
+++ src/train.ts
@@ -1,5 +1,35 @@
 /* 
 
+TASK V
+
+Masalaning sharti: 
+Shunday function yozing, uni string parametri bo'lsin.
+Va bu function stringdagi har bir harfni o'zi bilan
+necha marotaba taktorlanganligini ko'rsatuvchi object qaytarsin.
+  
+MASALAN: countChars("hello") return {h: 1, e: 1, l: 2, o: 1}
+
+Yuqoridagi misolda, 'hello' so'zi tarkibida
+qatnashgan harflar necha marotaba takrorlangini bilan
+object sifatida qaytarilmoqda.
+
+*/
+
+function countChars(str: string): Record<string, number> {
+  const result: Record<string, number> = {};
+
+  for (const char of str) {
+    result[char] = (result[char] || 0) + 1;
+  }
+
+  return result;
+}
+
+console.log(countChars("hello"));
+// { h: 1, e: 1, l: 2, o: 1 }
+
+/* 
+
 U - TASK 
 
 Masalaning sharti: 
@@ -11,7 +41,7 @@ MASALAN: sumOdds(9) return 4; sumOdds(11) return 5;
 
 Masalaning yechimi: 
 
-*/ 
+*
 
 function sumOdds(num: number): number {
   let count = 0;

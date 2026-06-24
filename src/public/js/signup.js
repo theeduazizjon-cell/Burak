2cdf7a2204cb93b682188f68b31023aceab516b2
Alex <theeduazizjon@gmail.com>
2026-06-24 16:35:23 +0900

    feat: Developed image uploading and SignUp validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,33 @@
 console.log("Signup frontend javascript file"); 
 
-$(function () {}); 
+$(function () {
+    constTarget =$(".file-box .upload-hidden");
+    let filename; 
+
+    fileTarget.on("change", function () {
+        if(window.FileReader) {
+            const uploadFile = $(this)[0].files[0]; 
+            console.log("uploadFile:", uploadFile); 
+            const fileType = uploadFile["type"];
+            const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+            
+            if(!validImageType.includes(fileType)) {
+                alert("Please insert only jpeg, jpg and png!"); 
+            }else{
+                if(uploadFile) {
+
+                    console.log(URL.createObjectURL(uploadFile)); 
+                    $(".upload-img-frame")
+                    .attr("src",URL.createObjectURL(uploadFile))
+                    .addClass("success"); 
+                }
+                filename = $(this)[0].files[0].name; 
+
+            }
+            $(this).siblings(".upload-name").val(filename);
+        }
+    });
+}); 
 
 function validateSignUpForm() {
     const memberNick = $(".member-nick").val(); 
@@ -22,4 +49,12 @@ function validateSignUpForm() {
           return false;
     }
    
+    const memberImage = $(".member-image").get(0).files[0].name
+        ? $(".member-image").get(0).files[0].name 
+        : null;
+    if (!memberImage) {
+        alert("Please insert restaurant image!"); 
+        return false; 
+    }
+
 }

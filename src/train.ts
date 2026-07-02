be44e5da905549175250b77adc238894dfb02c3b
Alex <theeduazizjon@gmail.com>
2026-07-02 20:18:15 +0900

    fix: algorithmic tasks on train.ts file

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,30 @@
+/* 
+
+TASK Z
+
+Masalaning sharti: 
+Shunday function yozing. Bu function sonlardan iborat array
+qabul qilsin. Function'ning vazifasi array tarkibidagi juft
+sonlarni topib ularni yig'disini qaytarsin.
+
+MASALAN:
+sumEvens([1, 2, 3]); return 2;
+sumEvens([1, 2, 3, 2]); return 4;
+
+Yuqoridagi misolda, bizning funktsiya
+berilayotgan array tarkibidagi sonlar ichidan faqatgina juft bo'lgan
+sonlarni topib, ularni hisoblab yig'indisini qaytarmoqda.
+
+*/ 
+
+function sumEvens(numbers: number[]): number {
+  return numbers
+    .filter((num) => num % 2 === 0)
+    .reduce((sum, num) => sum + num, 0);
+}
+
+console.log(sumEvens([1, 2, 3]));
+console.log(sumEvens([1, 2, 3, 2]));
 /*
 
 TASK Y
@@ -16,7 +43,7 @@ ikkala array'da ishtirok etgan o'xshash sonlarni yagona arrayga
 joylab return qilmoqda
 
 */ 
-
+/*
 function findIntersection(arr1: number[], arr2: number[]): number[] {
   const set2 = new Set(arr2);
   const result: number[] = [];

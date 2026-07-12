ab4b89a6f93daeca9bb59dc4f8620544942ef35a
Alex <theeduazizjon@gmail.com>
2026-07-12 22:51:45 +0900

    feat: Developed getProducts REST API

--- src/models/product.service.ts
+++ src/models/product.service.ts
@@ -4,9 +4,12 @@ import Errors, { HttpCode, Message } from "../libs/Error";
 import {
     Product,
     ProductInput,
+    ProductInquiry,
     ProductUpdateInput
 } from "../libs/types/product";
 import ProductModel from "../schema/product.model";
+import { ProductStatus } from "../libs/enums/product.enum";
+import { T } from "../libs/types/common";
 
 class ProductService {
     private readonly productModel; 
@@ -17,8 +20,33 @@ class ProductService {
 
     // SPA 
 
+    public async getProducts(inquiry: ProductInquiry): Promise<Product[]>{
+        console.log("inquiry", inquiry); 
+        const match: T = {productStatus: ProductStatus.PROCESS};
 
+        if(inquiry.productCollection) 
+            match.productCollect = inquiry.productCollection;
+        if(inquiry.search) {
+            match.productName = {$regex: new RegExp(inquiry.search, "i") }; 
+        }
 
+        const sort: T = inquiry.order === "productPrice" 
+        ? {[inquiry.order]:1} 
+        : {[inquiry.order]:-1}; 
+
+        const result = await this.productModel
+        .aggregate([
+            {$match: match}, 
+            {$sort: sort}, 
+            {$skip:(inquiry.page*1-1) * inquiry.limit}, // 0 
+            {$limit: inquiry.limit * 1},  // 3 
+
+        ])
+        .exec();
+        if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+        return [];
+    }
 
     // BSSR 
 
--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import express from 'express';
 const router = express.Router(); 
 import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader"; 
+import productController from './controllers/product.controller';
 
 //router.get('/', memberController.goHome); 
 
@@ -34,6 +35,8 @@ router.get(
 router.get("/member/top-users",memberController.getTopUsers);
 
 // => Product
+
+router.get("/product/all", productController.getProducts);
 
 // => Order
 export default router; // modele.exports

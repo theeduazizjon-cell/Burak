a8467f17bb12a66bd38dd83021d3ebd001ad88f5
Alex <theeduazizjon@gmail.com>
2026-07-13 00:43:28 +0900

    fix: Some spelling mistakes has been fixed

--- src/models/product.service.ts
+++ src/models/product.service.ts
@@ -100,7 +100,7 @@ class ProductService {
 
     // BSSR 
 
-    public async getAllProducts(memberId: mongoose.Schema.Types.ObjectId, id: string | string[]): Promise<Product[]> {
+    public async getAllProducts(): Promise<Product[]> {
         const result = await this.productModel.find().exec();
         if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
 

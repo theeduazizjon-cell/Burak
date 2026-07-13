d217d664d9b24702836c6ac473dc6e53eba2d517
Alex <theeduazizjon@gmail.com>
2026-07-13 22:56:20 +0900

    feat: Developed createOrder REST API

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader"; 
 import productController from './controllers/product.controller';
+import orderController from './controllers/order.controller';
 
 //router.get('/', memberController.goHome); 
 
@@ -44,4 +45,9 @@ router.get("/product/:id",
 );
 
 // => Order
+router.post("/order/create", 
+    memberController.verifyAuth, 
+    orderController.createOrder
+);
+
 export default router; // modele.exports
